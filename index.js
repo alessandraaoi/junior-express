51326348d5969cdbe279e6c9d97d2359b93f53f1
alessandraaoi <alexxandraoix.3@gmail.com>
2024-04-21 09:57:19 +0200

    changess

--- index.js
+++ index.js
@@ -21,4 +21,14 @@ connection.on('error', (err) => console.log('Error to connect: ' + (err)))
 
 app.use('/products', routerProducts); 
 
+// --------- Creo dos manejadores de errores 404 y 500 para la ruta: /products;
+routerProducts.use((req, res, next) => {
+    res.status(404).send('Ruta no encontrada, ERROR 404!');   
+})
+
+routerProducts.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('ERROR 500, ERROR EN EL SERVIDOR!');
+})
+
 app.listen(process.env.PORT, () => console.log('Started server'))
